12dc314f38f4bd2f9a27001da4cd73f6b2180f8e
shani geva <shanig7@gmail.com>
2024-01-21 19:26:52 +0200

    change the order

--- src/layout/myLinks.ts
+++ src/layout/myLinks.ts
@@ -10,14 +10,14 @@ const myLinks: Link[] = [
   { to: ROUTES.HOME, children: "Home page" },
   { to: ROUTES.REGISTER, children: "Register page" },
   { to: ROUTES.LOGIN, children: "Login page" },
-  { to: ROUTES.ABOUT, children: "About" },
   { to: ROUTES.TASKS, children: "Tasks" },
+  { to: ROUTES.ABOUT, children: "About" },
 ];
 // admin user
 const isAdminHeader: Link[] = [
-  { to: ROUTES.ABOUT, children: "About" },
   { to: ROUTES.TASKS, children: "Tasks" },
   { to: ROUTES.USERSLIST, children: "Users List" },
+  { to: ROUTES.ABOUT, children: "About" },
 ];
 // no user
 const loggedOutLinks: Link[] = [
@@ -27,9 +27,9 @@ const loggedOutLinks: Link[] = [
 ];
 // regular user
 const loggedInLinks: Link[] = [
-  { to: ROUTES.ABOUT, children: "About" },
   { to: ROUTES.TASKS, children: "Tasks" },
   { to: ROUTES.MYTASKS, children: "My Tasks" },
+  { to: ROUTES.ABOUT, children: "About" },
 ];
 
 export { myLinks, isAdminHeader, loggedInLinks, loggedOutLinks };

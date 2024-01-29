587d766f2a1b8eaa9534bbf5f3bfffa5f4a57f40
shani geva <shanig7@gmail.com>
2024-01-29 20:49:57 +0200

    add profile page

--- src/pages/profilePage/ProfilePage.tsx
+++ src/pages/profilePage/ProfilePage.tsx
@@ -1,58 +1,127 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import EditIcon from "@mui/icons-material/Edit";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { RootStateType } from "@/store/bigPie";
+import { User } from "@/lib/types";
+import FileUploadIcon from "@mui/icons-material/FileUpload";
+import EditUser from "../editUser/EditUser";
+import ROUTES from "@/Routes/ROUTES";
 
 const ProfilePage = () => {
+  const [editModalOpen, setEditModalOpen] = useState(false);
+  const [user, setUser] = useState<User | null>(null);
+  const userData = useSelector((bigPie: RootStateType) => bigPie.auth.userData);
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const userId = userData?.payload?._id;
+
+    if (userId) {
+      axios
+        .get(`/users/${userId}`)
+        .then(({ data }) => {
+          setUser(data.user);
+        })
+        .catch((error) => {
+          console.log("Error fetching user data", error);
+        });
+    }
+  }, [userData]);
+
+  const openEditModal = () => {
+    setEditModalOpen(true);
+  };
+
+  const closeEditModal = () => {
+    setEditModalOpen(false);
+  };
+
   return (
     <div className="flex items-center h-screen w-full justify-center">
-      <div className="max-w-xs">
-        <div className="bg-white shadow-xl rounded-lg py-3">
-          <div className="photo-wrapper p-2">
-            <img
-              className="w-32 h-32 rounded-full mx-auto"
-              src="https://www.gravatar.com/avatar/2acfb745ecf9d4dccb3364752d17f65f?s=260&d=mp"
-              alt="John Doe"
-            />
-          </div>
-          <div className="p-2">
-            <h3 className="text-center text-xl text-gray-900 font-medium leading-8">
-              John Doe
-            </h3>
-            <div className="text-center text-gray-400 text-xs font-semibold">
-              <p>Web Developer</p>
+      <div className="max-w-md w-full">
+        <h1 className="text-3xl mb-4 text-center">Profile</h1>
+        {user ? (
+          <div className="bg-white shadow-xl rounded-lg p-4">
+            <div className="photo-wrapper p-4 text-center">
+              {user?.profilePicture ? (
+                <img
+                  className="w-48 h-48 rounded-full mx-auto mb-4"
+                  src={user?.profilePicture}
+                  alt={`${user?.firstName} ${user?.lastName}`}
+                />
+              ) : (
+                <div className="mb-4">
+                  <img
+                    className="w-36 h-36 rounded-full mx-auto mb-2"
+                    src="/assets/images/avatar.png"
+                    alt="Default User"
+                  />
+                  <input
+                    type="file"
+                    className="file-input file-input-bordered file-input-xs w-full max-w-xs"
+                  />
+                </div>
+              )}
             </div>
-            <table className="text-xs my-3">
-              <tbody>
-                <tr>
-                  <td className="px-2 py-2 text-gray-500 font-semibold">
-                    Address
-                  </td>
-                  <td className="px-2 py-2">Chatakpur-3, Dhangadhi Kailali</td>
-                </tr>
-                <tr>
-                  <td className="px-2 py-2 text-gray-500 font-semibold">
-                    Phone
-                  </td>
-                  <td className="px-2 py-2">+977 9955221114</td>
-                </tr>
-                <tr>
-                  <td className="px-2 py-2 text-gray-500 font-semibold">
-                    Email
-                  </td>
-                  <td className="px-2 py-2">john@example.com</td>
-                </tr>
-              </tbody>
-            </table>
-
-            <div className="text-center my-3">
-              <a
-                className="text-xs text-indigo-500 italic hover:underline hover:text-indigo-600 font-medium"
-                href="#"
+            <div className="p-4">
+              <h3 className="text-center text-2xl text-gray-900 font-medium leading-8 mb-4">
+                {user?.firstName + " " + user?.lastName}
+              </h3>
+              <table className="text-base my-4 mx-auto">
+                <tbody>
+                  <tr>
+                    <td className="px-2 py-2 text-gray-500 font-semibold">
+                      Phone
+                    </td>
+                    <td className="px-2 py-2">{user?.phone}</td>
+                  </tr>
+                  <tr>
+                    <td className="px-2 py-2 text-gray-500 font-semibold">
+                      Email
+                    </td>
+                    <td className="px-2 py-2">{user?.email}</td>
+                  </tr>
+                  <tr>
+                    <td className="px-2 py-2 text-gray-500 font-semibold">
+                      Status
+                    </td>
+                    <td className="px-2 py-2">
+                      {user?.isAdmin ? "Admin" : "Regular"}
+                    </td>
+                  </tr>
+                </tbody>
+              </table>
+              <div
+                className="flex items-center justify-center space-x-2 my-4"
+                onClick={openEditModal}
               >
-                View Profile
-              </a>
+                <div className="text-gray-500 font-semibold cursor-pointer">
+                  Edit
+                </div>
+                <EditIcon className="text-indigo-500 cursor-pointer" />
+              </div>
+
+              <div className="text-center my-4">
+                <a
+                  className="text-base text-indigo-500 italic hover:underline hover:text-indigo-600 font-medium"
+                  onClick={() => navigate(ROUTES.TASKS)}
+                >
+                  Back Home
+                </a>
+              </div>
             </div>
           </div>
-        </div>
+        ) : (
+          <p className="text-xl text-center">User not found</p>
+        )}
       </div>
+      <EditUser
+        isModalOpen={editModalOpen}
+        closeModal={closeEditModal}
+        userId={userData?.payload?._id || ""}
+      />
     </div>
   );
 };

ebaa2b426bf175feb10897ddf01244807467097c
shani geva <shanig7@gmail.com>
2024-03-09 13:57:35 +0200

    fix style and responsive on login and register pages.

--- src/pages/registerPage/RegisterAuthPage.tsx
+++ src/pages/registerPage/RegisterAuthPage.tsx
@@ -5,7 +5,7 @@ const ResiterAuthPage = () => {
     <>
       <div className="container relative h-screen flex-col items-center justify-center md:grid lg:max-w-none lg:grid-cols-2 lg:px-0">
         <div className="relative hidden h-full flex-col p-10 text-white lg:flex dark:border-r">
-          <div className="absolute inset-0 bg-[url('/assets/images/blob-bgc.png')] bg-repeat-round " />
+          <div className="absolute inset-0 bg-[url('/assets/images/blob-bgc.png')] bg-no-repeat bg-cover" />
 
           <div className="z-20 flex items-center text-lg font-medium">
             Sunday{" "}
@@ -16,7 +16,7 @@ const ResiterAuthPage = () => {
             </p>
           </div>
         </div>
-        <div className="lg:p-8">
+        <div className="lg:p-8 md:p-8 pt-40">
           <div className="mx-auto flex w-full flex-col justify-center space-y-6 sm:w-[350px]">
             <div className="flex flex-col space-y-2 text-center">
               <h1 className="text-2xl font-semibold tracking-tight">

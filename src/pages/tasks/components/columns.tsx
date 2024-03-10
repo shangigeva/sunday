f0a7b0f23ed5e1586209ba0e789cb8c8648db678
shani geva <shanig7@gmail.com>
2024-03-10 20:06:29 +0200

    delete unused cose

--- src/pages/tasks/components/columns.tsx
+++ src/pages/tasks/components/columns.tsx
@@ -9,30 +9,6 @@ import { Badge } from "@/components/ui/badge";
 import UserSelectComponent from "./UserSelect";
 
 export const columns: ColumnDef<Task>[] = [
-  // {
-  //   id: "select",
-  //   header: ({ table }) => (
-  //     <Checkbox
-  //       checked={
-  //         table.getIsAllPageRowsSelected() ||
-  //         (table.getIsSomePageRowsSelected() && "indeterminate")
-  //       }
-  //       onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
-  //       aria-label="Select all"
-  //       className="translate-y-[2px]"
-  //     />
-  //   ),
-  //   cell: ({ row }) => (
-  //     <Checkbox
-  //       checked={row.getIsSelected()}
-  //       onCheckedChange={(value) => row.toggleSelected(!!value)}
-  //       aria-label="Select row"
-  //       className="translate-y-[2px]"
-  //     />
-  //   ),
-  //   enableSorting: false,
-  //   enableHiding: false,
-  // },
   {
     accessorKey: "TaskNumb",
     header: ({ column }) => (

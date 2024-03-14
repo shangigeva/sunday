e78c942be30b171379bbb0e527ffc6d2341d964e
shani geva <shanig7@gmail.com>
2024-03-14 11:23:24 +0200

    style

--- src/pages/tasks/Tasks.tsx
+++ src/pages/tasks/Tasks.tsx
@@ -51,14 +51,6 @@ const Tasks: React.FC = () => {
   return (
     <>
       <div className=" h-full flex-1 flex-col space-y-2 p-8 md:flex">
-        <div className="flex items-center justify-between space-y-2">
-          <div>
-            <h2 className="text-2xl font-bold tracking-tight">Welcome back!</h2>
-            <p className="text-muted-foreground">
-              Here&apos;s a list of your team tasks
-            </p>
-          </div>
-        </div>
         <DataTable data={tasks} columns={columns} />
         <Button onClick={openModal} className="bg-[#9584FF]">
           Create New Task
